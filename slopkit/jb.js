6c25c143bd75dcb566196ecf748d95bbfed4c002
MGstore96 <akuntvcoocaa12@gmail.com>
2026-09-23 02:03:23 +0700

    update slopkit/jb.js

--- slopkit/jb.js
+++ slopkit/jb.js
@@ -242,8 +242,7 @@ let allDone = false,
 
     const KPATCH_FILE =
       "slopkit/patches/" + (off.kpatch || fwKey.replace(".", "") + ".bin");
-    const PAYLOAD_FILE = "goldhen_v2.4b18.12.bin";
-    const needPatch = ["k_sysent_661", "k_jmp_rsi"].filter(
+    const PAYLOAD_FILE = "goldhen_v2.4b18.12.bin"].filter(
       (k) => off[k] === undefined,
     );
     if (
